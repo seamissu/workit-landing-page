0699d7d1a5f4c986e05db4bfe2cd9a5a8d6e48f0
Jingning <seamissu@gmail.com>
2023-11-07 22:44:51 +1100

    create HTML backbone

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,63 @@
 import Image from 'next/image';
+import Link from 'next/link';
+
 import styles from './page.module.css';
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className={styles.wrapper}>
+      <header className={styles.header}>
+        <div>
+          <Image alt="logo" />
+          <Link href="/">Apply for access</Link>
+        </div>
+        <p>Data tailored to your needs.</p>
+        <button>Learn more</button>
+      </header>
+
+      <main className={styles.main}>
+        <article>
+          <div>1</div>
+          <h2>Actionable insights</h2>
+          <p>
+            Optimize your products, improve customer satisfaction and
+            stay ahead of the competition with our product data
+            analytics.
+          </p>
+        </article>
+        <article>
+          <div>2</div>
+          <h2>Data-driven decisions</h2>
+          <p>
+            Make data-driven decisions with our product data
+            analytics. Our AI-generated reports help you unlock
+            insights hidden in your product data.
+          </p>
+        </article>
+        <article>
+          <div>3</div>
+          <h2>Always affordable</h2>
+          <p>
+            Always affordable pricing that scales with your business.
+            Get top-quality product data analytics services without
+            hidden costs or unexpected fees.
+          </p>
+        </article>
+      </main>
+      <section className={styles.profile}>
+        <Image alt="The founder image" />
+        <article>
+          <h2>Be the first to test</h2>
+          <p>
+            {`Hi, I'm Louis Graham, the founder of the company. Book a
+            demo call with me to become a beta tester for our app and
+            kickstart your company. Apply for access below and I’ll be
+            in touch to schedule a call.`}
+          </p>
+          <button>Apply for access</button>
+        </article>
+      </section>
+      <footer className={styles.footer}>Footer</footer>
+    </div>
+  );
 }
